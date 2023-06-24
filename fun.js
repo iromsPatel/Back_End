4e6dca40cac29ed5fdade4710570e3bf28df34f5
iromsPatel <rk254148@gmail.com>
2023-06-25 01:29:12 +0530

    js folder  import export

---
+++ fun.js
@@ -0,0 +1,8 @@
+module.exports.add=function(a,b){
+    return a+b;
+}
+// module.exports or exports <-- we use to export
+
+exports.multiplay=function(a,b){
+    return a*b;
+}
---
+++ funUse.js
@@ -0,0 +1,5 @@
+const accquireFun = require('./fun');
+// require file name 
+
+console.log(accquireFun.add( 55,55 ));
+console.log(accquireFun.multiplay( 55, 55 ));
